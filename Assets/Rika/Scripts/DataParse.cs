03f77ab5cfe5c1a6bb120422ca6fbb68afe57720
RikaSugisawa <tjj.rikap@gmail.com>
2020-02-02 02:12:36 +0900

    添加部分模型

---
+++ Assets/Rika/Scripts/DataParse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.CSharp;
+public class DataParse
+{
+    public static void parse(string logFile)
+    {
+        var rounds = JsonUtility.FromJson<dynamic>(logFile);
+        Debug.Log(rounds);
+    }
+}
---
+++ Assets/Rika/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DataParse.parse(File.ReadAllText(@"D:\学校\大学\软院\UI_THUAI\Test.json"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
