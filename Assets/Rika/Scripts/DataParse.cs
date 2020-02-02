716e4d48fb98e7f09412ba1676557bb17960c6b0
Starrah <46821571+Starrah@users.noreply.github.com>
2020-02-02 14:48:27 +0800

    Delete DataParse.cs

--- Assets/Rika/Scripts/DataParse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Microsoft.CSharp;
-public class DataParse {
-    public static void parse(string logFile) {
-
-    }
-}
