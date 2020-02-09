5f3d6fc2759db9ea31794149a957caa23b802459
Starrah <46821571+Starrah@users.noreply.github.com>
2020-02-09 15:41:19 +0800

    游戏开始的地图创建

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using GameData;
+using GameData.MapElement;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,12 +10,17 @@ public class GameControl : MonoBehaviour
     public static GameControl Instance = null;
     
     private GameDataSource _dataSource;
-    private List<GameObject>[][] _map;
+    private List<GameObject>[][] _gameMap;
 
     private float _time = 0.0f;
     public float timePerTurn = 0.5f;
     private int _currentTurn = -1;
     public int myAi = -1;//我的阵营编号
+
+    private GameObject _objPollution;
+    private GameObject _objBuilding;
+    private GameObject _objDetector;
+    private GameObject _objProcessor;
 
     public int CurrentTurn
     {
@@ -29,7 +35,7 @@ public class GameControl : MonoBehaviour
 
     T FindMapObject<T>(Vector2Int position) where T: MonoBehaviour
     {
-        foreach (var obj in _map[position.x][position.y])
+        foreach (var obj in _gameMap[position.x][position.y])
         {
             var component = obj.GetComponent<T>();
             if (component != null) return component;
@@ -67,6 +73,45 @@ public class GameControl : MonoBehaviour
     {
         _dataSource = new GameDataSource();
         Instance = GameObject.Find("GameControl").GetComponent<GameControl>();
+
+        var startData = _dataSource.GetStartData();
+        for (int x = 0; x < startData.MapWidth; x++)
+        {
+            for (int y = 0; y < startData.MapHeight; y++)
+            {
+                foreach (MapElementBase element in startData.Map[x][y].Elements)
+                {
+                    GameObject obj = null;
+                    if (element is PollutionSource ele1)
+                    {
+                        var control = Resources.Load<PollutionControl>("Prefabs/Pollution");
+                        obj = control.gameObject;
+                        control.SyncMapElementStatus(ele1);
+                    }else if (element is Building ele2)
+                    {
+                        var control = Resources.Load<BuildingControl>("Prefabs/Building");
+                        obj = control.gameObject;
+                        control.SyncMapElementStatus(ele2);
+                    }else if (element is Detector ele3)
+                    {
+                        var control = Resources.Load<DetectorControl>("Prefabs/Detector");
+                        obj = control.gameObject;
+                        control.SyncMapElementStatus(ele3);
+                    }else if (element is Processor ele4)
+                    {
+                        var control = Resources.Load<ProcessorControl>("Prefabs/Processor");
+                        obj = control.gameObject;
+                        control.SyncMapElementStatus(ele4);
+                    }
+
+                    var pos = obj.transform.position;
+                    pos.x = x;
+                    pos.z = y;
+                    obj.transform.position = pos;
+                    
+                }
+            }
+        }
     }
 
     // Update is called once per frame
