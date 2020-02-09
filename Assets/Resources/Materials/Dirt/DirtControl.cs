f9810efb454165ef1953f2536652fc2f249f7984
RikaSugisawa <tjj.rikap@gmail.com>
2020-02-09 22:06:06 +0900

    添加地块模型及其相关脚本

---
+++ Assets/Resources/Materials/Dirt/DirtControl.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GameData;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class DirtControl : MonoBehaviour {
+    private MapPlace _place = new MapPlace(new System.Numerics.Vector2(0, 0));
+    private MeshRenderer _meshRenderer;
+    private Material _material;
+
+    private Color _color {
+        get {
+            var c = (int) (_place.Position.X + _place.Position.Y) % 2 == 0 ? Color.white : new Color(.8f, .8f, .8f);
+            if (_place.Type == MapPlaceTypes.BOUGHT)
+                c *= (_place.Owner == 0 ? new Color(1, .6f, .6f) : new Color(.6f, .6f, 1));
+            return c;
+        }
+    }
+
+    public MapPlace Place {
+        get => _place;
+        set {
+            _place = value;
+            transform.position = new Vector3(value.Position.X, 0, value.Position.Y);
+            //如果出价状态非空（即至少进入拍卖环节
+            if (!(value.Bid is null)){
+                switch (value.Type){
+                    case MapPlaceTypes.CANNOT_BUY:
+                        break;
+                    case MapPlaceTypes.EMPTY:
+                        //如果还没拍成
+                        if (GetComponentInChildren<TextMeshPro>() is null)
+                            Instantiate(Resources.Load<TextMeshPro>("Materials/Dirt/Num"), transform);
+                        var tmp = GetComponentInChildren<TextMeshPro>();
+                        tmp.text = value.Bid.turn.ToString();
+                        tmp.color = new Color(value.Bid.Ai == 0 ? 1 : 0, 0, value.Bid.Ai == 1 ? 1 : 0, .5f);
+                        break;
+                    case MapPlaceTypes.BOUGHT_FAILED:
+                        if (GetComponentInChildren<TextMeshPro>() is null)
+                            Instantiate(Resources.Load<TextMeshPro>("Materials/Dirt/Num"), transform);
+                        tmp = GetComponentInChildren<TextMeshPro>();
+                        tmp.text = "×";
+                        tmp.color = new Color(value.Bid.Ai == 0 ? 1 : 0, 0, value.Bid.Ai == 1 ? 1 : 0, .5f);
+                        break;
+                    case MapPlaceTypes.BOUGHT:
+                        //如果买到手了
+                        if (!(GetComponentInChildren<TextMeshPro>() is null))
+                            Destroy(GetComponentInChildren<TextMeshPro>().gameObject);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            if (_material) _material.color = _color;
+        }
+    }
+
+    private void Start(){
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _material = new Material(_meshRenderer.material);
+        _meshRenderer.material = _material;
+        _material.mainTextureOffset = new Vector2(Random.Range(0, 1f), Random.Range(0, 1f));
+        _material.color = _color;
+    }
+}
