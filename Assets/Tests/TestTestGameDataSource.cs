bfa5b89183b1a01a02867b815ec8f6691900688a
RikaSugisawa <tjj.rikap@gmail.com>
2020-02-11 21:26:33 +0900

    基本相机控制

--- Assets/Tests/TestTestGameDataSource.cs
+++ Assets/Tests/TestTestGameDataSource.cs
@@ -1,19 +1,27 @@
 ﻿using System;
+using GameData;
+using UnityEngine;
 using UnityEngine.Assertions;
 
-namespace Tests
-{
-    public class TestTestGameDataSource : UnityEngine.MonoBehaviour
-    {
+namespace Tests {
+    public class TestTestGameDataSource : UnityEngine.MonoBehaviour {
         public TestGameDataSource ds;
-        private void Start()
-        {
-            Generate();
+
+        private void Start(){
+            // Generate();
+            generateDirts();
         }
 
+        private void generateDirts(){
+            var dirtPrefab = Resources.Load<DirtControl>("Prefabs/Dirt");
+            for (int i = 0; i < 25; i++)
+                for (int j = 0; j < 25; j++){
+                    var dirt = Instantiate(dirtPrefab);
+                    dirt.Place = new MapPlace(new Vector2Int(i, j));
+                }
+        }
 
-        public void Generate()
-        {
+        public void Generate(){
             ds = new TestGameDataSource();
             ds.ReadFile("");
             Assert.IsTrue(true);
