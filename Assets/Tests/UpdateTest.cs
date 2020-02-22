8959f70b4707bbd65118b1196b047046e677f42a
Starrah <46821571+Starrah@users.noreply.github.com>
2020-02-22 16:49:27 +0800

    UI调整位置

---
+++ Assets/Tests/UpdateTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Tests
+{
+    public class UpdateTest : MonoBehaviour
+    {
+        void Update()
+        {
+            var ins = GameControl.Instance;
+            var a = 1;
+        }
+    }
+}
