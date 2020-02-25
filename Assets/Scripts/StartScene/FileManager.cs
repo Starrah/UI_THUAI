f660c3cb35bfe1fa5ac5a29334e7d932885377eb
Starrah <46821571+Starrah@users.noreply.github.com>
2020-02-25 16:24:45 +0800

    创建开始场景 提供阵营选择和文件选择、异步化加载游戏数据文件与进度实时显示等方法 优化了一些地方 TODO：初始界面的背景素材

---
+++ Assets/Scripts/StartScene/FileManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace StartScene
+{
+
+    /**
+     * 引用：https://blog.csdn.net/SimulationPD/article/details/83027801
+     */
+//脚本FileManager
+    public static class FileManager
+    {
+        private static OpenFileName CreateFileNameObj(string str, string filter)
+        {
+            OpenFileName openFileName = new OpenFileName();
+            openFileName.structSize = Marshal.SizeOf(openFileName);
+            //文件类型 config配置文件,"Excel文件(*.xlsx)\0*.xlsx" ,"Txt文件(*.txt)\0*.txt"...
+            openFileName.filter = filter;
+            openFileName.file = new string(new char[256]);//new一个256字符的string
+            openFileName.maxFile = openFileName.file.Length;//获取256字符的string的长度作为最大
+            openFileName.fileTitle = new string(new char[64]);//64字符的string
+            openFileName.maxFileTitle = openFileName.fileTitle.Length;//文件标题的最大长度
+            openFileName.initialDir = UnityEngine.Application.dataPath;//默认路径
+            openFileName.title = str;//文件标题
+            openFileName.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000008;
+            return openFileName;
+        }
+ 
+        public static OpenFileName OpenFile(string str, string filter)
+        {
+            OpenFileName openFileName = CreateFileNameObj(str, filter);
+            if (LocalDialog.GetOpenFileName(openFileName))
+            {
+                return openFileName;
+            }
+            else
+            {
+                throw new Exception("打开文件失败！请检查文件是否存在。");
+            }
+        }
+ 
+        public static OpenFileName SaveFile(string str, string filter)
+        {
+            OpenFileName openFileName = CreateFileNameObj(str, filter);
+            if (LocalDialog.GetSaveFileName(openFileName))
+            {
+                return openFileName;
+            }
+            else
+            {
+                throw new Exception("保存文件失败！请检查路径是否合法存在。");
+            }
+        }
+    }
+    
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public class OpenFileName
+    {
+        public int structSize = 0;
+        public IntPtr dlgOwner = IntPtr.Zero;
+        public IntPtr instance = IntPtr.Zero;
+        public String filter = null;
+        public String customFilter = null;
+        public int maxCustFilter = 0;
+        public int filterIndex = 0;
+        public String file = null;
+        public int maxFile = 0;
+        public String fileTitle = null;
+        public int maxFileTitle = 0;
+        public String initialDir = null;
+        public String title = null;
+        public int flags = 0;
+        public short fileOffset = 0;
+        public short fileExtension = 0;
+        public String defExt = null;
+        public IntPtr custData = IntPtr.Zero;
+        public IntPtr hook = IntPtr.Zero;
+        public String templateName = null;
+        public IntPtr reservedPtr = IntPtr.Zero;
+        public int reservedInt = 0;
+        public int flagsEx = 0;
+    }
+ 
+    public class LocalDialog
+    {
+ 
+        //链接指定系统函数       打开文件对话框
+        [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+        public static extern bool GetOpenFileName([In, Out] OpenFileName ofn);
+
+        //链接指定系统函数        另存为对话框
+        [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+        public static extern bool GetSaveFileName([In, Out] OpenFileName ofn);
+    }
+}
---
+++ Assets/Scripts/StartScene/StartSceneControl.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GameData;
+using StartScene;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartSceneControl : MonoBehaviour
+{
+
+    private int _myAi;
+    public GameObject RedFlagObj;
+    public GameObject BlueFlagObj;
+    public GameObject Settings;
+    public GameObject Loadings;
+    public Text LoadingText;
+    public GameObject LoadingSureButton;
+    
+    private GameDataSource _gameDataSource;
+    private bool _flagLoadingDataProcessing = false;
+
+    private bool FlagLoadingDataProcessing
+    {
+        get => _flagLoadingDataProcessing;
+        set
+        {
+            _flagLoadingDataProcessing = value;
+            LoadingSureButton.SetActive(!value);
+        }
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnClickRed();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FlagLoadingDataProcessing && _gameDataSource != null)
+            LoadingText.text = "读取游戏数据中......" + Math.Round(_gameDataSource.ProcessedRate * 100) + "%";
+    }
+
+    public void OnClickExceptionReaded()
+    {
+        Settings.SetActive(true);
+        Loadings.SetActive(false);
+    }
+    
+    public void OnClickRed()
+    {
+        Debug.Log("RedClicked");
+        _myAi = 0;
+        RedFlagObj.transform.localScale = new Vector3(1.5f, 1.5f,  1);
+        BlueFlagObj.transform.localScale = new Vector3(1, 1, 1);
+        RedFlagObj.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+        BlueFlagObj.GetComponent<Image>().color = new Color(255, 255, 255, 100);
+    }
+
+    public void OnClickBlue()
+    {
+        Debug.Log("BlueClicked");
+        _myAi = 1;
+        RedFlagObj.transform.localScale = new Vector3(1, 1,  1);
+        BlueFlagObj.transform.localScale = new Vector3(1.5f, 1.5f, 1);
+        RedFlagObj.GetComponent<Image>().color = new Color(255, 255, 255, 100);
+        BlueFlagObj.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+    }
+
+    public void OnClickGameStart()
+    {
+        StartCoroutine(OnClickGameStartAsync());
+    }
+    
+    public IEnumerator OnClickGameStartAsync()
+    {
+        Debug.Log("clk");
+        Settings.SetActive(false);
+        Loadings.SetActive(true);
+        LoadingText.text = "读取游戏数据中......0%";
+        OpenFileName fileInfo = null;
+
+        try
+        {
+            fileInfo = FileManager.OpenFile("打开文件", "游戏数据文件(*.json)\0*.json");
+            if (fileInfo == null || string.IsNullOrEmpty(fileInfo.file)) throw new Exception("打开文件失败，请检查文件是否存在！");
+            _gameDataSource = new GameDataSource();
+            FlagLoadingDataProcessing = true;
+        }
+        catch (Exception e)
+        {
+            FlagLoadingDataProcessing = false;
+            LoadingText.text = "错误：" + e.Message;
+            yield break;
+        }
+        
+        IEnumerator enumerator = null;
+        try
+        {
+            enumerator = _gameDataSource.ReadFileAsync(fileInfo.file);
+        }
+        catch (Exception e)
+        {
+            FlagLoadingDataProcessing = false;
+            LoadingText.text = "错误：" + e.Message;
+            yield break;
+        }
+        yield return null;
+        
+        bool res = true;
+        while (res)
+        {
+            try
+            {
+                res = enumerator.MoveNext();
+            }
+            catch (Exception e)
+            {
+                FlagLoadingDataProcessing = false;
+                LoadingText.text = "错误：" + e.Message;
+                yield break;
+            }
+            yield return null;
+        }
+
+        try
+        {
+            GameControl.DataPassedFromStartScene = (_gameDataSource, _myAi);
+            SceneManager.LoadScene("Main");
+        }catch (Exception e)
+        {
+            FlagLoadingDataProcessing = false;
+            LoadingText.text = "错误：" + e.Message;
+            yield break;
+        }
+    }
+}
