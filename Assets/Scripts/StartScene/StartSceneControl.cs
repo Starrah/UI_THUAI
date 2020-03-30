5c9f7a364b7575dba9e3b41bb5c4c4e5238aacd8
Starrah <46821571+Starrah@users.noreply.github.com>
2020-03-30 22:38:39 +0800

    OSX平台特殊处理：不调用系统选择文件对话框，而是直接在根目录搜索.json文件

--- Assets/Scripts/StartScene/StartSceneControl.cs
+++ Assets/Scripts/StartScene/StartSceneControl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using GameData;
 using StartScene;
 using UnityEngine;
@@ -83,20 +84,52 @@ public class StartSceneControl : MonoBehaviour
         Loadings.SetActive(true);
         LoadingText.text = "读取游戏数据中......0%";
         OpenFileName fileInfo = null;
+        _gameDataSource = new GameDataSource();
+        
+        //条件编译以实现不同系统下打开文件功能
+#if UNITY_STANDALONE_WIN
+        // //Windows调用系统dll打开文件对话框
+        // try
+        // {
+        //     fileInfo = FileManager.OpenFile("打开文件", "游戏数据文件(*.json)\0*.json");
+        //     if (fileInfo == null || string.IsNullOrEmpty(fileInfo.file)) throw new Exception("打开文件失败，请检查文件是否存在！");
+        //     FlagLoadingDataProcessing = true;
+        // }
+        // catch (Exception e)
+        // {
+        //     FlagLoadingDataProcessing = false;
+        //     LoadingText.text = "错误：" + e.Message;
+        //     yield break;
+        // }
+#endif
 
+#if  UNITY_STANDALONE_WIN
+        //Mac OS X直接搜索json文件
         try
         {
-            fileInfo = FileManager.OpenFile("打开文件", "游戏数据文件(*.json)\0*.json");
-            if (fileInfo == null || string.IsNullOrEmpty(fileInfo.file)) throw new Exception("打开文件失败，请检查文件是否存在！");
-            _gameDataSource = new GameDataSource();
+            string path = ".";
+            DirectoryInfo root = new DirectoryInfo(path);
+            List<string> jsonList = new List<string>();
+            foreach (FileInfo f in root.GetFiles())//遍历文件夹下的每个文件
+            {
+                string filename = path + "\\" + f.Name;//得到单个文件的filename，自己进行相关操作
+                if (filename.Substring(filename.Length - 5, 5) == ".json")
+                {
+                    jsonList.Add(filename);
+                }
+            }
+            jsonList.Sort();
+            if (jsonList.Count <= 0) throw new Exception("没有找到文件，请将.json播放文件放置于当前目录下！");
+            fileInfo = new OpenFileName(){file = jsonList[jsonList.Count - 1]};
             FlagLoadingDataProcessing = true;
-        }
-        catch (Exception e)
+        }catch (Exception e)
         {
             FlagLoadingDataProcessing = false;
             LoadingText.text = "错误：" + e.Message;
             yield break;
         }
+        
+#endif
         
         IEnumerator enumerator = null;
         try
